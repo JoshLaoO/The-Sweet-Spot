03974260fdf3aafd4becad3c68497c4a00e22ef4
annalucky888 <143918110+annalucky888@users.noreply.github.com>
2024-01-26 12:30:56 -0800

    step 3 finished

--- ghi/src/header.js
+++ ghi/src/header.js
@@ -2,20 +2,37 @@ import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 
-function Header() {
+function Header({ isLoggedIn, userType, userName }) {
     return (
         <div className="header-container">
             <div className="header-title">Sweet-Spot</div>
             <input type="text" className="search-box" placeholder="Search for candies" />
-
             <div className="nav-buttons">
-
                 <Link to="/mainpage/" className="button-link">Sweet Home</Link>
 
+                {!isLoggedIn && (
+                    <>
+                        <Link to="/signup/" className="button-link">Sign Up</Link>
+                        <Link to="/login/" className="button-link">Log In</Link>
+                    </>
+                )}
 
-                <Link to="/signup/" className="button-link">Sign Up</Link>
-                <Link to="/login/" className="button-link">Log In</Link>
+                {isLoggedIn && userType === 'user' && (
+                    <>
+                        <span>Hello, {userName}</span>
+                        <Link to="/logout/" className="button-link">Log Out</Link>
+                    </>
+                )}
 
+                {isLoggedIn && userType === 'business' && (
+                    <>
+                        <span>Hello, {userName}</span>
+                        <Link to="/profile/" className="button-link">Profile</Link>
+                        <Link to="/orders/" className="button-link">Orders</Link>
+                        <Link to="/transactions/" className="button-link">Transactions</Link>
+                        <Link to="/logout/" className="button-link">Log Out</Link>
+                    </>
+                )}
 
                 <Link to="/cart/" className="button-link">
                     <i className="fa fa-shopping-cart"></i> Cart
