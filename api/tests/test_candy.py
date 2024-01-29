fdab4bfd728a5cde8a5a962bc6885b6a1c29d542
Brian Argenti <brian.h.argenti@gmail.com>
2024-01-29 16:24:45 -0600

    fixes formatting

--- api/tests/test_candy.py
+++ api/tests/test_candy.py
@@ -1,8 +1,9 @@
 from fastapi.testclient import TestClient
 from main import app
-from queries.candy import CandyRepository, CandyOut
+from queries.candy import CandyRepository
 
 client = TestClient(app)
+
 
 class EmptyCandyRepository:
     def get_all(self):
@@ -12,54 +13,56 @@ class EmptyCandyRepository:
 class CreateCandyQueries:
     def create(self, candy):
         result = {
-                  "id": 0,
-                  "name": "string",
-                  "business": 0,
-                  "picture_url": "string",
-                  "description": "string",
-                  "price": 0,
-                  "stock": 0
-                  }
-        result.update(candy)
-        return result
-
-def test_get_candy():
-    #Arrange
-    app.dependency_overrides[CandyRepository] = EmptyCandyRepository
-    #Act
-    response = client.get("/candy")
-    app.dependency_overrides = {}
-
-    #Assert
-    assert response.status_code == 200
-    assert response.json() == []
-
-def test_create_candy():
-    #Arrange
-    app.dependency_overrides[CandyRepository] = CreateCandyQueries
-    candy = {
+            "id": 0,
             "name": "string",
             "business": 0,
             "picture_url": "string",
             "description": "string",
             "price": 0,
-            "stock": 0
-            }
+            "stock": 0,
+        }
+        result.update(candy)
+        return result
 
-    expected = {
-                "id": 0,
-                "name": "string",
-                "business": 0,
-                "picture_url": "string",
-                "description": "string",
-                "price": 0,
-                "stock": 0
-                }
 
-    #Act
-    response = client.post("/candy",json=candy)
+def test_get_candy():
+    # Arrange
+    app.dependency_overrides[CandyRepository] = EmptyCandyRepository
+    # Act
+    response = client.get("/candy")
     app.dependency_overrides = {}
 
-    #Assert
+    # Assert
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+def test_create_candy():
+    # Arrange
+    app.dependency_overrides[CandyRepository] = CreateCandyQueries
+    candy = {
+        "name": "string",
+        "business": 0,
+        "picture_url": "string",
+        "description": "string",
+        "price": 0,
+        "stock": 0,
+    }
+
+    expected = {
+        "id": 0,
+        "name": "string",
+        "business": 0,
+        "picture_url": "string",
+        "description": "string",
+        "price": 0,
+        "stock": 0,
+    }
+
+    # Act
+    response = client.post("/candy", json=candy)
+    app.dependency_overrides = {}
+
+    # Assert
     assert response.status_code == 200
     assert response.json() == expected
