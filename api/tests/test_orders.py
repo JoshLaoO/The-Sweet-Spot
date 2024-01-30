3f50dff0353babd7cee2c80320777b432616763a
Josh <jgarlao@gmail.com>
2024-01-29 18:12:03 -0600

    flake8

--- api/tests/test_orders.py
+++ api/tests/test_orders.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
-from queries.orders import OrderRepo, OrderIn
+from queries.orders import OrderRepo
 from queries.candy import CandyRepository
 
 client = TestClient(app)
