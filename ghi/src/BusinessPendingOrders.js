36afe86002abc6ed307041ad1e4dd25c36ce633c
Skylar <skylarastout@gmail.com>
2024-02-01 11:51:23 -0800

    wip pending orders

--- ghi/src/BusinessPendingOrders.js
+++ ghi/src/BusinessPendingOrders.js
@@ -6,35 +6,35 @@ function BusinessPendingOrders() {
     async function getOrders() {
         const url = "http://localhost:8000/orders"
         const response = await fetch(url)
-        if (response.ok){
-            const data = await response.json();
-            setOrders(data.orders)
+        if (response.ok) {
+            const data = fetch(url).then(res => res.clone().json())
+            setOrders(data.id)
             // console.log(orders)
             console.log(response.json())
         }
     }
 
 
-useEffect(() => {
-    getOrders()
-}
-)
+    useEffect(() => {
+        getOrders()
+    }
+    )
 
-return (
-    <>
-        <div className="mb-3 mt-3 d-grid gap-2 d-md-flex justify-content-md-end">
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>Order</th>
-                    <th>Customer Information</th>
-                    <th>Quantity</th>
-                    <th>Total</th>
-                    <th>Ship</th>
-                </tr>
-            </thead>
-            <tbody>
-                {/* {
+    return (
+        <>
+            <div className="mb-3 mt-3 d-grid gap-2 d-md-flex justify-content-md-end">
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Order</th>
+                            <th>Customer Information</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                            <th>Ship</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
                     orders?.map((order) => {
                         return (
                             <tr key={order.id}>
@@ -42,26 +42,26 @@ return (
                             </tr>
                         )
                     })
-                } */}
-                <tr>
-                    <td>1</td>
-                    <td>Customer 1</td>
-                    <td>10</td>
-                    <td>25</td>
-                    <td>
-                        <button
+                }
+                        <tr>
+                            <td>1</td>
+                            <td>Customer 1</td>
+                            <td>10</td>
+                            <td>25</td>
+                            <td>
+                                <button
 
-                            type="button"
+                                    type="button"
 
-                        >
-                            Ship
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        </div>
-    </>
+                                >
+                                    Ship
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 export default BusinessPendingOrders;
