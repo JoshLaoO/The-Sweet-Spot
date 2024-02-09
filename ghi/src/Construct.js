79eb0c601a2b3980d6e0c2a988199c04a4cc9290
Josh <jgarlao@gmail.com>
2024-02-09 16:01:29 -0600

    Construct replaced with candy image

--- ghi/src/Construct.js
+++ ghi/src/Construct.js
@@ -1,4 +1,4 @@
-
+import candySaleImage from './images/candysale.png';
 
 function Construct(props) {
   let info = props.info;
@@ -11,18 +11,7 @@ function Construct(props) {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Under construction</h1>
-        <h2>Coming on (or before)</h2>
-        <h2>
-          Module: {props.info.module} Week: {props.info.week} Day:{" "}
-          {props.info.day}
-        </h2>
-        <h2>
-          by or <strong>WELL BEFORE</strong> {props.info.hour}:{props.info.min}{"  "}
-          Pacific Time
-        </h2>
-      </header>
+      <img src={candySaleImage} />
     </div>
   );
 }
