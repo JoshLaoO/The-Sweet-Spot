7a59c72825fdddb523084fce0a65399f1054ec74
Brian Argenti <brian.h.argenti@gmail.com>
2024-02-14 21:56:41 +0000

    Delete user

--- ghi/src/header.js
+++ ghi/src/header.js
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { changeToken } from './features/token/tokenSlice';
 import { useNavigate } from 'react-router-dom';
 
-function Header({ isLoggedIn, userType, userName }) {
+function Header() {
     const [username, setUsername] = useState()
     const [id, setId] = useState()
     const navigate = useNavigate()
@@ -59,26 +59,23 @@ function Header({ isLoggedIn, userType, userName }) {
     /* eslint-enable */
     return (
         <div className="header-container">
-            <div className="header-title">Sweet-Spot</div>
+            <Link to="/mainpage/" className="btn btn-info header-title pacifico-regular">TheSweetSpot</Link>
             <div className="w-25 p-2">
                 <form onSubmit={handleSearch} className="d-flex" role="search">
                     <i className="fa-solid fa-magnifying-glass m-2"></i>
                     <input onChange={(e) => setSearch(e.target.value)} type="text" className="form-control me-2" placeholder="Search by candy name" aria-label="Search" />
                 </form>
             </div>
-            <div className="nav-buttons">
-                <Link to="/mainpage/" className="btn btn-info text-white m-2">Sweet Home</Link>
-
-
 
                 {token.length > 0 ?
                     <>
-                        <span>Hello, {username}</span>
+                        <h1 className='honk-banner-name'>Hello, {username}</h1>
                         <button onClick={logout} className="btn btn-danger m-2">Log Out</button>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <Link to="/users" className="btn btn-info te    xt-white m-2">Connect!</Link>
+                        <Link to={`/users/user/${id}`} onClick={navigateToMyPage} className="btn btn-info text-white m-2">Me</Link>
                         {business &&
                             <Link to='/mybusiness' className="btn btn-success text-white m-2">Inventory</Link>
                         }
@@ -88,18 +85,7 @@ function Header({ isLoggedIn, userType, userName }) {
                         <Link to="/login/" className="btn btn-info text-white m-2">Log In</Link>
                     </>
                 }
-
-                {isLoggedIn && userType === 'business' && (
-                    <>
-                        <span>Hello, {userName}</span>
-                        <Link to="/profile/" className="btn btn-info text-white">Profile</Link>
-                        <Link to="/orders/" className="btn btn-info text-white">Orders</Link>
-                        <Link to="/transactions/" className="btn btn-info text-white">Transactions</Link>
-
-                        <button className="btn btn-danger m-2">Log Out</button>
-                    </>
-                )}
-                <Link to={`/users/user/${id}`} onClick={navigateToMyPage} className="btn btn-info text-white m-2">Me</Link>
+                <Link to="/orders" className="btn btn-info text-white m-2">Pending Orders</Link>
                 <Link to="/cart/" className="btn btn-info text-white">
                     <i className="fa fa-shopping-cart"></i> Cart
                 </Link>
@@ -108,7 +94,6 @@ function Header({ isLoggedIn, userType, userName }) {
 
 
             </div>
-        </div>
     );
 }
 
