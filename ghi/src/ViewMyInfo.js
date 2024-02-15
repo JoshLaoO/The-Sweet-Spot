5bebe2dcf01245d46bf579de03c85a9a4dbb8e20
Brian Argenti <brian.h.argenti@gmail.com>
2024-02-14 20:16:44 -0600

    reformats user page

--- ghi/src/ViewMyInfo.js
+++ ghi/src/ViewMyInfo.js
@@ -80,28 +80,3 @@ function ViewMyInfo(props) {
 }
 
 export default ViewMyInfo;
-
-{/* <>
-    <h1 style={{ color: "darkorange" }}>{data.username}'s Profile Page!</h1>
-    <div className="col-md-4  mb-3 d-flex justify-content-center">
-        <div className="col mb-3 shadow" style={{ background: "darkorange" }}>
-            <div className="p-3">
-                <div className="text-center">
-                    <img src={data.picture_url} alt="user profile" className="rounded" style={{ width: "200px", height: "200px" }} />
-                </div>
-                <div className="card-body">
-                    <h4 className='card-title' style={{ color: "white" }}>Username: {data.username}</h4>
-                    <h4 className='card-title' style={{ color: "white" }}>Email: {data.email}</h4>
-                    {business ?
-                        <>
-                            <h4 className='card-title' style={{ color: "white" }}>Business: {business.business_name}</h4>
-                            <h4 className='card-title' style={{ color: "white" }}>Business Email: {business.business_email}</h4>
-                        </>
-                        : <h4 className='card-title' style={{ color: "white" }}>Customer Account</h4>}
-                    <button onClick={() => deleteMyAccount(routeParams.userId)} className="btn btn-danger m-2" style={{ float: 'right' }}>Delete</button>
-                    <Link to={`/users/user/${routeParams.userId}/edit`} style={{ float: 'right' }} className="btn btn-info text-white m-2">Edit</Link>
-                </div>
-            </div>
-        </div>
-    </div>
-</> */}
